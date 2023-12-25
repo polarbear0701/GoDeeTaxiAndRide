1ee3efe1d7325c3fd7469f1ff96c78a89b963195
Deyuhnat <netpro2344@gmail.com>
2023-12-25 23:44:12 +0700

    profilepage

---
+++ app/src/main/java/com/example/godee/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.godee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        getSupportActionBar().hide();
+    }
+}
