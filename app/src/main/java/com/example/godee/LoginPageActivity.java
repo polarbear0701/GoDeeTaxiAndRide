55e88833dc4de6e9a95639c1fd2352a9b3921e75
AnhDoanNep <140772322+AnhDoanNep@users.noreply.github.com>
2023-12-25 22:43:55 +0700

    login UI

---
+++ app/src/main/java/com/example/godee/LoginPageActivity.java
@@ -0,0 +1,32 @@
+package com.example.godee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        TextView textView;
+
+        getSupportActionBar().hide();
+
+        textView = findViewById(R.id.registerNow);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
