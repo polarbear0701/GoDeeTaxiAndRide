8a87a480d113d40f5d17547ac3555f9b4850f413
thaiho <thaiho500@gmail.com>
2023-12-28 17:52:16 +0700

    Create new Bottom Navigation bar Associate File: + navigation_menu.xml + page_navigation_home.xml + page_navigation_profile.xml + page_navigation_outline.xml

--- app/src/main/java/com/example/godee/ProfileActivity.java
+++ app/src/main/java/com/example/godee/ProfileActivity.java
@@ -2,7 +2,12 @@ package com.example.godee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.checkerframework.common.subtyping.qual.Bottom;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,7 +15,34 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
         getSupportActionBar().hide();
+
+        BottomNavigationView pageMenu = findViewById(R.id.page_navigation);
+        pageMenu.setSelectedItemId(R.id.activity_profile);
+        pageNavigation(pageMenu);
     }
+    // Function for page navigation (bottom navigation bar)
+    public void pageNavigation(BottomNavigationView pageMenu) {
+        pageMenu.setOnItemSelectedListener(item ->
+        {   int itemId = item.getItemId();
+            if (itemId == R.id.activity_home){
+                // Start new activity base on the item selected on the navigation bar
+                // If the current tab is equal to the selected item only need to set this if-statement to return true
+                return true;
+            }
+            if (itemId == R.id.activity_profile){
+                Intent intent = new Intent(getApplicationContext(),ProfileActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            // This part is expandable base on the number of tab need for the application
+            /* Structure
+               if (itemId == R.id.[name of the item]){
+               Intent intent = new Intent(getApplicationContext(), [insert the activity associated with the icon].class);
+               startActivity(intent);
+               finish();}
+             */
+            return true;
+        });
+    };
 }
