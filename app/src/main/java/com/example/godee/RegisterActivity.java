97cc71806f1176af95190866566edfdb20f04ff1
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2023-12-28 12:56:48 +0700

    add sign up features using firebase

--- app/src/main/java/com/example/godee/RegisterActivity.java
+++ app/src/main/java/com/example/godee/RegisterActivity.java
@@ -1,11 +1,21 @@
 package com.example.godee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -17,7 +27,15 @@ public class RegisterActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         TextView textView;
+        EditText inputEmail, inputPassword, inputConfirmPassword;
+
+        inputEmail = findViewById(R.id.email);
+        inputPassword = findViewById(R.id.password);
+        inputConfirmPassword = findViewById(R.id.passwordConfirmation);
+
         textView = findViewById(R.id.loginNow);
+        Button registerButton = findViewById(R.id.btn_register);
+
 
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +43,39 @@ public class RegisterActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), LoginPageActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password, confirm;
+                email = String.valueOf(inputEmail.getText());
+                password = String.valueOf(inputPassword.getText());
+                confirm = String.valueOf(inputConfirmPassword.getText());
+
+                if (!(email.isEmpty()) && !(password.isEmpty()) && !(confirm.isEmpty()) && password.equals(confirm)) {
+                    signUpFunction(email, password);
+                }
+                else{
+                    Toast.makeText(RegisterActivity.this, "Fail to register!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    private void signUpFunction(String userEmail, String userPassword){
+        FirebaseAuth userAccount = FirebaseAuth.getInstance();
+        userAccount.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(RegisterActivity.this, "Sign up account successfully", Toast.LENGTH_SHORT).show();
+                    Intent backToLogin = new Intent(RegisterActivity.this, LoginPageActivity.class);
+                    startActivity(backToLogin);
+                    finish();
+                }
             }
         });
     }
