c73d9d8e02f77feaa78e7f0b35aa93966a938930
kakakaka2410 <49601822+kakakaka2410@users.noreply.github.com>
2024-01-04 01:06:03 +0700

    modify name display in ProfileActivity.java

--- app/src/main/java/com/example/godee/ProfileActivity.java
+++ app/src/main/java/com/example/godee/ProfileActivity.java
@@ -4,29 +4,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    Button logoutButton;
+    private Button logoutButton;
+    private TextView emailDisplay, nameDisplay;
 
     FirebaseAuth auth;
 
     FirebaseUser user;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        ProgressBar progressBar = findViewById(R.id.progressBar);
 
         setContentView(R.layout.activity_profile);
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
+
+        emailDisplay = findViewById(R.id.profileEmail);
+        nameDisplay = findViewById(R.id.profileName);
+
+        db.collection("users").document(auth.getUid()).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                emailDisplay.setText(task.getResult().get("email").toString());
+                nameDisplay.setText(task.getResult().get("name").toString());
+            }
+//            else{
+//                progressBar.setVisibility(View.GONE);
+//            }
+        });
 
         getSupportActionBar().hide();
 
