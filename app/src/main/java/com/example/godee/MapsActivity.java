47c851c9ef4e5bbbd98eca81677975a02db33f48
kakakaka2410 <49601822+kakakaka2410@users.noreply.github.com>
2024-01-06 01:03:31 +0700

    add find the route feature when searching (not yet delete previous marker)

--- app/src/main/java/com/example/godee/MapsActivity.java
+++ app/src/main/java/com/example/godee/MapsActivity.java
@@ -24,7 +24,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
