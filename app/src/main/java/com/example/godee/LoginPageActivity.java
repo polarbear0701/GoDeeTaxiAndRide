b67db34faaf52268336b3a4ddec85f9f85b7af45
kakakaka2410 <49601822+kakakaka2410@users.noreply.github.com>
2024-01-08 21:08:12 +0700

    modify login activity

--- app/src/main/java/com/example/godee/LoginPageActivity.java
+++ app/src/main/java/com/example/godee/LoginPageActivity.java
@@ -1,26 +1,19 @@
 package com.example.godee;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.util.Objects;
 
 public class LoginPageActivity extends AppCompatActivity {
@@ -47,7 +40,7 @@ public class LoginPageActivity extends AppCompatActivity {
         Button loginButton;
         EditText emailSignIn, passwordSignIn;
 
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         loginButton = findViewById(R.id.btn_login);
         textView = findViewById(R.id.registerNow);
@@ -56,57 +49,65 @@ public class LoginPageActivity extends AppCompatActivity {
 
         TextView tvDriverLogin = findViewById(R.id.tvDriverLogin);
 
-        tvDriverLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginPageActivity.this, DriverLoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        tvDriverLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginPageActivity.this, DriverLoginActivity.class);
+            startActivity(intent);
+            finish();
         });
 
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent returnUser = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivity(returnUser);
-                finish();
-            }
+        textView.setOnClickListener(v -> {
+            Intent returnUser = new Intent(getApplicationContext(), RegisterActivity.class);
+            startActivity(returnUser);
+            finish();
         });
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email, password;
+        loginButton.setOnClickListener(v -> {
+            String email;
+            String password;
+            hideKeyboard(v);
 
-                email = String.valueOf(emailSignIn.getText());
-                password = String.valueOf(passwordSignIn.getText());
 
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
-                DocumentReference documentReference = db.collection("users").document("EcWs461VgHPFZ6IKieTWp7rOkh23");
-                documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+            email = String.valueOf(emailSignIn.getText());
+            password = String.valueOf(passwordSignIn.getText());
+
+            if (email.isEmpty() || password.isEmpty()){
+                //Toast empty
+                Toast.makeText(LoginPageActivity.this, "Please fill in all the fields!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()){
+
+                    DocumentReference documentReference = db.collection("users").document(Objects.requireNonNull(mAuth.getUid()));
+                    documentReference.get().addOnSuccessListener(documentSnapshot -> {
                         UserModel modelTemp = documentSnapshot.toObject(UserModel.class);
+                        assert modelTemp != null;
                         if (modelTemp.getAccountType() != 100){
                             Toast.makeText(LoginPageActivity.this, "You are a driver, please login in driver page!", Toast.LENGTH_SHORT).show();
+                            mAuth.signOut();
                         }
                         else{
-                            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
-                                if (task.isSuccessful()){
-                                    Toast.makeText(LoginPageActivity.this, "Login successfully!", Toast.LENGTH_SHORT).show();
-                                    Intent toHomePage = new Intent(LoginPageActivity.this, MapsActivity.class);
-                                    startActivity(toHomePage);
-                                    finish();
-                                }
-                                else{
-                                    Toast.makeText(LoginPageActivity.this, "Login failed!", Toast.LENGTH_SHORT).show();
-                                }
-                            });
+                            Toast.makeText(LoginPageActivity.this, "Login successfully!", Toast.LENGTH_SHORT).show();
+                            Intent toHomePage = new Intent(LoginPageActivity.this, MapsActivity.class);
+                            startActivity(toHomePage);
+                            finish();
                         }
-                    }
-                });
-            }
+                    });
+
+                }
+                else{
+                    Toast.makeText(LoginPageActivity.this, "Login failed!", Toast.LENGTH_SHORT).show();
+                }
+            });
         });
     }
+
+    private void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
 }
