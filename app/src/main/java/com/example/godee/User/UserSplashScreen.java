6b67b374b11c6df1bd4189b24d43f6509851bd94
thaiho <thaiho500@gmail.com>
2024-01-14 20:18:59 +0700

    Update User Splash Screen

---
+++ app/src/main/java/com/example/godee/User/UserSplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.godee.User;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.godee.R;
+
+public class UserSplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_splash_screen);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginPageActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
